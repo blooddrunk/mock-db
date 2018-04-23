727f1d68296515e6384cb85374ccc8812ebadad0
Ryder Xie <jelinenaro@gmail.com>
2018-04-23 10:35:55 +0800

    two more fields for boilerplate db

---
+++ mock/mock.config.js
@@ -0,0 +1,43 @@
+const url = require('url');
+
+// const getPagination = (requestUrl, res) => {
+//   if (Array.isArray(res.locals.data)) {
+//     let { _limit: limit, _page: page } = requestUrl.query;
+//     limit = Number.parseInt(limit, 10);
+//     page = Number.parseInt(page, 10);
+//     const total = Number.parseInt(res.getHeader('X-Total-Count'), 10);
+//     const hasMore = total > (page + 1) * limit;
+//     return hasMore;
+//   }
+// };
+
+const getPagination = res => {
+  const result = res.locals.data;
+  const total = Number.parseInt(res.getHeader('X-Total-Count'), 10);
+  return {
+    total,
+    items: result,
+  };
+};
+
+const render = (req, res) => {
+  const requestUrl = url.parse(req.url, true);
+
+  let result;
+  switch (requestUrl.pathname) {
+    case '/posts':
+      result = getPagination(res);
+      break;
+    default:
+      result = res.locals.data;
+  }
+  res.jsonp(result);
+};
+
+module.exports = {
+  render,
+  routes: {
+    '/api/*': '/$1',
+  },
+  dbPath: './db.js',
+};
